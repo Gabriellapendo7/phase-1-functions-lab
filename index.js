7b9df394a24e3eb5e4e5da1d5853b5091ad084de
gabriellapendo7 <gabriellapendo12@gmail.com>
2024-03-24 19:30:54 +0300

    changes

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    return Math.abs(blocks - 42);
+}
+
+function distanceFromHqInFeet(blockNumber) {
+    const headquartersBlock = 42;
+    const feetPerBlock = 264;
+    let distance = Math.abs(headquartersBlock - blockNumber) * feetPerBlock;
+    return distance;
+}
+
+function distanceTravelledInFeet(startingBlock, endingBlock) {
+    const feetPerBlock = 264;
+    return Math.abs(endingBlock - startingBlock) * feetPerBlock;
+}
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    let price = 0;
+    if (distance <= 400) {
+        price = 0;
+    } else if (distance > 400 && distance <= 2000) {
+        price = (distance - 400) * 0.02;
+    } else if (distance <= 2500) {
+        price = 25;
+    } else {
+        return 'cannot travel that far';
+    }
+    return price;
+
+}
